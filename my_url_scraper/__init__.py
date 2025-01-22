55a4f20bad49c0e27dc4c3fa9aef49e953c8c460
Andrés Fernández Burón <193231832+andres123dev@users.noreply.github.com>
2025-01-22 21:53:22 +0100

    Update __init__.py

--- my_url_scraper/__init__.py
+++ my_url_scraper/__init__.py
@@ -3,16 +3,14 @@
 
 __name__ =        "my_url_scraper"
 __description__ = "Interactive console app to scrape and analyze the response for an URI request"
-__date__ =        "14-06-2022"
 __author__ =      "Andrés Fernández Burón"
 __copyright__ =   "Copyright 2022-2025"
 __license__ =     "All rights reserved"
+__date__ =        "14-06-2022"
 __version__ =     "0.1"
 __status__ =      "Production"
 __maintainer__ =  "Andrés Fernández Burón"
 __email__ =       "https://github.com/andres123dev/my-url-scraper/issues/new/choose"
-
-__name__ = 'my_url_scraper'
 
 __all__ = [
     'files',
