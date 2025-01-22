3851c7955c3783415312859149e5c39fb082dbc9
Andrés Fernández Burón <193231832+andres123dev@users.noreply.github.com>
2025-01-22 21:52:45 +0100

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -3,8 +3,8 @@
 
 __name__ =        "__main__"
 __description__ = "Main script of the interactive console app to scrape and analyze the response for an URI request."
-__date__ =        "14-06-2022"
 __author__ =      "Andrés Fernández Burón"
+__date__ =        "14-06-2022"
 __copyright__ =   "Copyright 2022-2025"
 __license__ =     "All rights reserved"
 __version__ =     "0.1"
@@ -12,7 +12,7 @@ __status__ =      "Production"
 __maintainer__ =  "Andrés Fernández Burón"
 __email__ =       "https://github.com/AndresFernandezBuron/my-url-scraper/issues/new/choose"
 
-from my_url_scraper import *
+from my_url_scraper import * 
 
 # MUESTRO LA CABECERA
 menu.print_header()
